374b51bc3447072c83ed2c4327f251487c37d1e6
Kumar Harsh <kumar.harsh492@gmail.com>
2023-09-26 20:17:33 +0530

    first commitit

---
+++ WpfTutorialSeries/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using ControlzEx.Theming;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTutorialSeries
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickEvent(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Welcome to Wpf Tutorial series!!!");
+        }
+
+        private void Register(object sender, RoutedEventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=EmployeeDB;Integrated Security=True");
+            connection.Open();
+            SqlCommand command = new SqlCommand("insert into Employee (EmpID,EmpName,DOB,Address,Country,Gender,PhotoUrl) values" +
+                "(@EmpID,@EmpName,@DOB,@Address,@Country,@Gender,@PhotoUrl)", connection);
+            var empId = Convert.ToInt32(txtEmpID.Text);
+            var dob = Convert.ToDateTime(dpDOB.Text);
+            var gender = (bool)rbMale.IsChecked ? "Male" : "Female";
+            command.Parameters.AddWithValue("@EmpID", empId);
+            command.Parameters.AddWithValue("@EmpName", txtName.Text);
+            command.Parameters.AddWithValue("@DOB", dob);
+            command.Parameters.AddWithValue("@Address", txtAddress.Text);
+            command.Parameters.AddWithValue("@Country", cbCountry.SelectedItem);
+            command.Parameters.AddWithValue("@Gender", gender);
+            command.Parameters.AddWithValue("@PhotoUrl", "");
+            command.ExecuteNonQuery();
+            MessageBox.Show("Sucessfully Saved", "Sucess");
+        }
+
+        private void GetDetails(object sender, RoutedEventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=EmployeeDB;Integrated Security=True");
+            connection.Open();
+            SqlCommand command = new SqlCommand("select * from Employee where EmpID=@EmpID", connection);
+            var empId = Convert.ToInt32(txtEmpID.Text);
+            command.Parameters.AddWithValue("@EmpID", empId);
+            var reader=command.ExecuteReader();
+            while(reader.Read())
+            {
+                txtName.Text = reader["EmpName"].ToString();
+                dpDOB.Text = reader["DOB"].ToString();
+                txtAddress.Text = reader["Address"].ToString();
+                var country = reader["Country"].ToString();
+                var gender = reader["Gender"].ToString();
+                cbCountry.SelectedItem = country;
+                if (gender == "Male")
+                    rbMale.IsChecked = true;
+                else
+                    rbFemale.IsChecked = true;
+            }
+
+        }
+
+        private void Update(object sender, RoutedEventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=EmployeeDB;Integrated Security=True");
+            connection.Open();
+            SqlCommand command = new SqlCommand("update Employee set EmpID=@EmpID,EmpName=@EmpName," +
+                "DOB=@DOB,Address=@Address,Country=@Country,Gender=@Gender,PhotoUrl=@PhotoUrl where EmpID=@EmpID", connection);
+            var empId = Convert.ToInt32(txtEmpID.Text);
+            var dob = Convert.ToDateTime(dpDOB.Text);
+            var gender = (bool)rbMale.IsChecked ? "Male" : "Female";
+            command.Parameters.AddWithValue("@EmpID", empId);
+            command.Parameters.AddWithValue("@EmpName", txtName.Text);
+            command.Parameters.AddWithValue("@DOB", dob);
+            command.Parameters.AddWithValue("@Address", txtAddress.Text);
+            command.Parameters.AddWithValue("@Country", cbCountry.SelectedItem);
+            command.Parameters.AddWithValue("@Gender", gender);
+            command.Parameters.AddWithValue("@PhotoUrl", "");
+            command.ExecuteNonQuery();
+            MessageBox.Show("Sucessfully Updated", "Sucess");
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=EmployeeDB;Integrated Security=True");
+            connection.Open();
+            SqlCommand command = new SqlCommand("delete from Employee where EmpID=@EmpID", connection);
+            var empId = Convert.ToInt32(txtEmpID.Text);
+            command.Parameters.AddWithValue("@EmpID", empId);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Sucessfully deleted", "Sucess");
+        }
+
+        private void ChengeThemeToDark(object sender, RoutedEventArgs e)
+        {
+            ThemeManager.Current.ChangeTheme(this, "Dark.Blue");
+        }
+
+        private void ChengeThemeToLight(object sender, RoutedEventArgs e)
+        {
+            ThemeManager.Current.ChangeTheme(this, "Light.Blue");
+        }
+    }
+}
---
+++ WpfTutorialSeries/ResourceWin.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfTutorialSeries
+{
+    /// <summary>
+    /// Interaction logic for ResourceWin.xaml
+    /// </summary>
+    public partial class ResourceWin : Window
+    {
+        public ResourceWin()
+        {
+            InitializeComponent();
+            //this.Resources["ButtonBackground"] = new SolidColorBrush(Colors.Green);
+
+        }
+
+        private void ChangeColor(object sender, RoutedEventArgs e)
+        {
+            //SolidColorBrush buttonColor = (SolidColorBrush)FindResource("ButtonBackground");
+            //buttonColor.Color = Colors.Red;
+            Resources["ButtonBackground"] = new SolidColorBrush(Colors.Red);
+        }
+
+        private void GreenColor(object sender, RoutedEventArgs e)
+        {
+            //SolidColorBrush buttonColor = (SolidColorBrush)FindResource("ButtonBackground");
+            //buttonColor.Color = Colors.Green;
+            Resources["ButtonBackground"] = new SolidColorBrush(Colors.Green);
+        }
+
+        private void BlueColor(object sender, RoutedEventArgs e)
+        {
+            //SolidColorBrush buttonColor = (SolidColorBrush)FindResource("ButtonBackground");
+            //buttonColor.Color = Colors.Blue;
+            Resources["ButtonBackground"] = new SolidColorBrush(Colors.Blue);
+        }
+    }
+}
